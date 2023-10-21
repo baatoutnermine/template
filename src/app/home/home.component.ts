ba19739d452cae9e6eada3b3bf3a0acf4d9d3217
nermine <baatoutnermine@gmail.com>
2023-10-22 01:19:16 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
